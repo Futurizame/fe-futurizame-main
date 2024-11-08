93e445696d0cb2987e315eba8206a88f8edad97a
Will Estrada <will@futuriza.me>
2024-11-08 16:29:15 -0500

    improved SEO

--- src/assets/js/title.js
+++ src/assets/js/title.js
@@ -1,58 +1,49 @@
+const titleEle = document.getElementById("main-title");
+const originalText = "Futuriza Me";
+
 const LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const randomLetter = () => {
   return LETTERS[Math.floor(Math.random() * LETTERS.length)];
 };
 
-const i01 = document.getElementById("i01");
-const i02 = document.getElementById("i02");
-const i03 = document.getElementById("i03");
-const i04 = document.getElementById("i04");
-const i05 = document.getElementById("i05");
-const i06 = document.getElementById("i06");
-const i07 = document.getElementById("i07");
-const i08 = document.getElementById("i08");
-const i09 = document.getElementById("i09");
-const i10 = document.getElementById("i10");
-
-const elementsLetter = [
-  { element: i01, letter: "F" },
-  { element: i02, letter: "u" },
-  { element: i03, letter: "t" },
-  { element: i04, letter: "u" },
-  { element: i05, letter: "r" },
-  { element: i06, letter: "i" },
-  { element: i07, letter: "z" },
-  { element: i08, letter: "a" },
-  { element: i09, letter: "M" },
-  { element: i10, letter: "e" },
-];
-
-const initRandomLetterAnimation = (ele, letter, endTimestamp) => {
+const startAnimation = (timeConfig, endTimestamp) => {
   const intervalId = setInterval(() => {
-    if (Date.now() >= endTimestamp) {
+    const now = Date.now();
+
+    titleEle.textContent = timeConfig
+      .map((endAt, index) => {
+        if (originalText[index] === " " || now >= endAt) {
+          return originalText[index];
+        }
+
+        return randomLetter();
+      })
+      .join("");
+
+    if (now >= endTimestamp) {
       clearInterval(intervalId);
-      ele.textContent = letter;
-
-      return;
     }
-
-    ele.textContent = randomLetter();
   }, 100);
+};
+
+const buildTimeConfig = (length, endTimestamp) => {
+  return Array.from(new Array(length), (_, index) => {
+    const deltaMs = (length - (index + 1)) * 400;
+
+    return endTimestamp - deltaMs;
+  });
 };
 
 const startTitleAnimation = (durationMs) => {
   const startTimestamp = Date.now();
+  const endTimestamp = startTimestamp + durationMs;
 
-  const length = elementsLetter.length;
+  const length = originalText.length;
 
-  elementsLetter.forEach(({ element, letter }, index) => {
-    const deltaMs = (length - (index + 1)) * 400;
+  const timeConfig = buildTimeConfig(length, endTimestamp);
 
-    const endTimestamp = startTimestamp + durationMs - deltaMs;
-
-    initRandomLetterAnimation(element, letter, endTimestamp);
-  });
+  startAnimation(timeConfig, endTimestamp);
 };
 
 setTimeout(() => {
