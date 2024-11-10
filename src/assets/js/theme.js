76223aaa5b8e08e74f77edea03c89598a9ef2031
Will Estrada <will@futuriza.me>
2024-11-10 17:38:27 -0500

    added theme switcher

--- src/assets/js/theme.js
+++
@@ -1,9 +0,0 @@
-const themeButton = document.getElementById("theme-button");
-
-const changeColorScheme = () => {
-  const root = document.querySelector(":root");
-
-  root.style.setProperty("--color-on-background", "239 242 19");
-};
-
-themeButton.addEventListener("click", changeColorScheme);
